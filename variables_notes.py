1d7340da9f20643afeaf68cba18f89e7ef169ce3
Dotting990 <malaki.araiza@ucas-edu.net>
2026-09-09 14:52:14 -0600

    variables_notes.py

---
+++ variables_notes.py
@@ -0,0 +1,8 @@
+# VL, Variables Notes <= for people to read not the computer
+
+name = input("Tell me your name: ")
+
+print("I like CSP")
+print("Hello")
+print("Hi", name, "How are you")
+print(name)
