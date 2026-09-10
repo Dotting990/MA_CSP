364c2697e28e905e31830f47283d24d2fab0b60c
Dotting990 <malaki.araiza@ucas-edu.net>
2026-09-10 14:47:19 -0600

    Variables Practice

--- variables_practice.py
+++ variables_practice.py
@@ -1,15 +1,15 @@
 # MA, Variables Practice
 
-myname = ("Malaki")
-favnumber = ("7")
-from100to1000 = ("61")
-whatieat = ("A apple")
-colorilike = ("Purple")
-oldschool = ("FreedompprepAcademy")
-yearCE = ("2026")
-eyepigment = ("Brown")
-malakiage = ("15")
-preferredsubject = ("Math")
+myname = "Malaki"
+favnumber = "7"
+from100to1000 = "61"
+whatieat = "A apple"
+colorilike = "Purple"
+oldschool = "FreedompprepAcademy"
+yearCE = "2026"
+eyepigment = "Brown"
+malakiage = "15"
+preferredsubject = "Math"
 
 print(myname)
 print(favnumber)
