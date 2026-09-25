47f8360259d62abc92a88b93776b9bb7d3761360
Dotting990 <malaki.araiza@ucas-edu.net>
2026-09-24 22:39:14 -0600

    Update Password_Strength_Checker.py

--- Password_Strength_Checker.py
+++ Password_Strength_Checker.py
@@ -5,11 +5,52 @@ uppercase = False
 lowercase = False
 number = False  
 symbol = False
+count = 0
+strength = "week"
+if len(Password) >= 8:
+    length = True
+print(f"Password has at least 8 characters {length}")
+
 for letter in Password:
     if letter.isupper():
         uppercase = True
-else:
-    break
-for letter in password
+print(f"Password has at least one uppercase letter {uppercase}")
+        
+for letter in Password:
     if letter.islower():
         lowercase = True
+print(f"Password has at least one lowercase letter {lowercase}")
+
+for letter in Password:
+    if letter.isnumeric:
+        number = True
+print(f"Password has at least one number")
+
+for  letter in Password
+    if letter in "@$?!:;()/-.,&[]{}#%^*+=_\|~<>"
+        symbol = true
+print(f"Password has a symbol")
+
+if length == True:
+    count = count + 1
+    
+if uppercase == True:
+    count = count + 1
+    
+if lowercase == True:
+    count = count + 1
+    
+if number == True:
+    count = count + 1
+    
+if symbol == True:
+    count = count + 1
+
+if count < 2
+    print(f"Your password is Weak")
+    
+if count > 3 and <= 4
+ print(f"Your password is Midium")
+
+if count == 5
+    print(f"Your password is Strong")
